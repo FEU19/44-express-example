4ab15d185735a5c85082f59c5697ae91990c0622
David Andersson <lejonmanen@gmail.com>
2020-08-18 15:57:50 +0200

    Web server has API endpoint with simple frontend script

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,20 @@
 // script.js
+window.addEventListener('load', () => {
+    let buttonGetContacts = document.querySelector('#buttonGetContacts');
+    let contactList = document.querySelector('#contactList')
+
+    buttonGetContacts.addEventListener('click', async () => {
+        // send request to backend - to get all contacts
+        // create DOM elements to show the contacts on the page
+        const response = await fetch('/api/contacts', { method: 'GET' });
+        const object = await response.json();  // motsvarar JSON.parse
+
+        console.log('Fetch returned:', object);
+        contactList.innerHTML = '';
+        object.forEach(contact => {
+            let li = document.createElement('li');
+            li.innerHTML = `${contact.name} - ${contact.email}`;
+            contactList.appendChild(li)
+        })
+    })
+})
--- server.js
+++ server.js
@@ -5,7 +5,21 @@ const port = 1237;
 // Middleware
 app.use( express.static(__dirname + '/frontend') )
 
+// Data (ska hämtas från databas i normala fall)
+// Person-objekt: { name, email }
+let data = [
+    { name: 'Nisse', email: 'nisse@exempel.se' },
+    { name: 'Anna', email: 'ananas@exempel.se' }
+];
+
 // Routes
+// GET /api/contacts -> ska returnera alla kontakter
+app.get('/api/contacts', (req, res) => {
+    res.send(data);
+})
+
+// TODO: lägg till flera routes, för GET/POST/PUT/DELETE
+
 
 // (error handling)
 
